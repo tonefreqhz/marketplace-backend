49c67a78306a23f2a342fe26000a495382d5a50c
4dcoder <getsnigeria@gmail.com>
2018-07-17 16:44:28 +0100

    remove duplicate files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": [2, "single", { "avoidEscape": true }],
+        "linebreak-style": 0,
+        "no-console": 0,
+        "no-unused-vars": 1,
+        "no-param-reassign": 0,
+        "eslint linebreak-style": [0, "error", "windows"],
+        "no-tabs": 0,
+        "object-curly-newline": [
+            "error",
+            {
+              "ObjectExpression": {
+                "multiline": true,
+                "consistent": true
+              },
+              "ObjectPattern": {
+                "multiline": true,
+                "consistent": true
+              }
+            }
+          ]
+    }
+};
